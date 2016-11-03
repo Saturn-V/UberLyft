cc6f46d8d29325ed058340dfbb65530e716c5235
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-11-03 13:10:14 -0700

    getting heroku ready

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
+  console.log('Message received');
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
