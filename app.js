6931e8dbbf86e94c8d45d2dddf9a1a85ffb88794
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-11-03 22:40:40 -0700

    fixed environment variable reference

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
 // URL where the app is running (include protocol). Used to point to scripts and
 // assets located at this address.
 // const SERVER_URL = (process.env.MESSENGER_SERVER_URL);
-const SERVER_URL = (process.env.SERVER_URL) ?
-    process.env.SERVER_URL :
+const SERVER_URL = (process.env.MESSENGER_SERVER_URL) ?
+    process.env.MESSENGER_SERVER_URL :
     config.get('serverURL');
 
 
